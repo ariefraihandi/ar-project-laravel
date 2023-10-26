3bec13d2a029e77a5a7a14d4bddcecc0ab488bbc
ariefraihandi <raihandi93@gmail.com>
2023-10-26 14:10:06 +0700

    fsafv

--- resources/views/Emails/payment-success.blade.php
+++ resources/views/Emails/payment-success.blade.php
@@ -103,7 +103,7 @@
 
 <body style="background-color: #f4f4f4; margin: 0 !important; padding: 0 !important;">
     <!-- HIDDEN PREHEADER TEXT -->
-    <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;"> Selamat!, Makalah Anda Siap Untuk Didownload.
+    <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;"> Selamat!, File Anda Siap Untuk Didownload.
     </div>
     <table border="0" cellpadding="0" cellspacing="0" width="100%">
         <!-- LOGO -->
@@ -132,7 +132,7 @@
                 <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                     <tr>
                         <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                            <p style="margin: 0;">Terimaksih Sudah Melakukan Pembayaran, Silahkan Download Makalah Anda Melalui Tombol Dibawah Ini</p>
+                            <p style="margin: 0;">Terimaksih Sudah Melakukan Pembayaran, Silahkan Download File Anda Melalui Tombol Dibawah Ini</p>
                         </td>
                     </tr>
                     <tr>
@@ -142,7 +142,7 @@
                                     <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 60px 30px;">
                                         <table border="0" cellspacing="0" cellpadding="0">
                                             <tr>
-                                                <td align="center" style="border-radius: 3px;" bgcolor="#5f4dee"><a href="{{ $tokenUrl }}" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #5f4dee; display: inline-block;">Download Makalah</a></td>
+                                                <td align="center" style="border-radius: 3px;" bgcolor="#5f4dee"><a href="{{ $tokenUrl }}" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #5f4dee; display: inline-block;">Download File</a></td>
                                             </tr>
                                         </table>
                                     </td>
